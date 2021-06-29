5d8e30554e22eeac0a9adf605aefde2e720d7c57
asjacobs92 <asjacobs92@inf.ufrgs.br>
2021-06-29 19:35:19 -0300

    Support for reading zipped weights file.

--- skexplain/utils/persist.py
+++ skexplain/utils/persist.py
@@ -1,21 +1,34 @@
+from copy import Error
 import os
+import zipfile
 import traceback
 
 from joblib import dump, load
 
 
 def load_model(path):
-    """ Loads model weights, if they exist """
-    if path and os.path.isfile(path):
+    """Loads model weights, if they exist"""
+    file_path = path
+    if zipfile.is_zipfile(path):
+        file_dir = os.path.dirname(path)
+        file_path = path.replace(".zip", "")
+        zip_ref = zipfile.ZipFile(path)  # create zipfile object
+        zip_ref.extractall(file_dir)  # extract file to dir
+        zip_ref.close()  # close file
+
+    if file_path and os.path.isfile(file_path):
         try:
-            model = load(path)
+            model = load(file_path)
+            if zipfile.is_zipfile(path):
+                os.remove(file_path)  # delete unzipped file
+
             return model
-        except:
+        except Error:
             traceback.print_exc()
             return None
     return None
 
 
 def save_model(model, path):
-    """ Saves model weights """
+    """Saves model weights"""
     return dump(model, path)
