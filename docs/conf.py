f2a2fa188511243b798b60aabcb7b8e2ef8c9282
Arthur Jacobs <asjacobs92@inf.ufrgs.br>
2022-08-22 20:45:50 -0300

    Trying to fix docs issues.

--- docs/conf.py
+++ docs/conf.py
@@ -20,23 +20,14 @@ templates_path = ["_templates"]
 exclude_patterns = []
 
 
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(".."))
+
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+
 html_theme = "furo"
 html_static_path = ["_static"]
-
-
-autodoc_mock_imports = [
-    "numpy",
-    "scipy",
-    "pandas",
-    "scikit-learn",
-    "matplotlib",
-    "rootpath",
-    "setuptools",
-    "torch",
-    "prettytable",
-    "termcolor",
-    "autogluon",
-]
